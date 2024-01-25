f46dd87661071fda6ecf0ec518f130f95151ba06
NT5K <nicholastysh@gmail.com>
2024-01-25 12:36:30 -0500

    fix api fetch url

--- client/src/pages/Game.jsx
+++ client/src/pages/Game.jsx
@@ -24,8 +24,8 @@ const Game = () => {
 
     const fetchMovie = async () => {
         try {
-            const apiUrl = import.meta.env.VITE_REACT_APP_API_URL;
-            const response = await axios.get(`${apiUrl}/movie?title=${movieTitle}`);
+            // const apiUrl = import.meta.env.VITE_REACT_APP_API_URL;
+            const response = await axios.get(`movie?title=${movieTitle}`);
             if (response.data.Response === 'False') {
                 setMovieData(null);
                 setErrorMessage(response.data.Error);
