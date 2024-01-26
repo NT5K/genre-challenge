99e8a949329f0c232e612484777e20bc2a725848
NT5K <nicholastysh@gmail.com>
2024-01-26 14:38:10 -0500

    created api folder, updated vercel.json file

--- server/api/index.js
+++ server/api/index.js
@@ -5,27 +5,24 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
-const PORT = 3001;
-const corsOptions = {
-  origin: 'http://localhost:5173', // Replace with your client's domain
-  optionsSuccessStatus: 200
-};
+const PORT = process.env.PORT || 3001;
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send({message: "home"});
+});
 
 app.get('/movie', async (req, res) => {
   const { title } = req.query;
-  // console.log('Title:', title); // Log the title
   try {
     const url = `https://www.omdbapi.com/?t=${title}&apikey=${process.env.API_KEY}`;
     const response = await axios.get(url);
-    console.log('Response:', response.data); 
     res.json(response.data);
   } catch (error) {
-    console.error('Error:', error); // Log the error
-    res.status(500).send('Error fetching movie data');
+    console.error('Error:', error);
+    res.status(500).send('Error fetching movie data', error);
   }
 });
 
